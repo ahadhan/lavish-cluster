a65f8feda7db0e1fcc05dbb42316749e7b13fb9a
Abdul Ahad Khan <ahadkhanmay@gmail.com>
2024-10-26 05:29:20 -0700

    remove stripe version

--- app/api/checkout/route.js
+++ app/api/checkout/route.js
@@ -104,9 +104,7 @@ import { NextResponse } from 'next/server';
 import nodemailer from 'nodemailer';
 
 // Initialize Stripe with your secret key and specify the API version
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2024-09-30', // Update to your Stripe API version
-});
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export const runtime = 'nodejs';
 
